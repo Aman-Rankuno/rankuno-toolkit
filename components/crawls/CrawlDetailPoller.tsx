934e03e59e35a8aab4328e3a2f7cbd72815a5313
Aman Bharti <aman.bharti@rankuno.com>
2026-06-17 16:25:19 +0530

    feat: add Overview Report placeholder button to data exports

--- components/crawls/CrawlDetailPoller.tsx
+++ components/crawls/CrawlDetailPoller.tsx
@@ -59,6 +59,7 @@ type ExportItem = {
 };
 
 const EXPORTS: ExportItem[] = [
+  { icon: FileBarChart,     label: "Overview Report" },
   { icon: FileText,        label: "Response Codes Internal", slug: "response-codes-internal" },
   { icon: AlertCircle,     label: "URL Issues",              slug: "url-issues" },
   { icon: Heading,         label: "Page Titles",             slug: "page-titles" },
